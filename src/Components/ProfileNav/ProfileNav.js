701d9ae847cb6f79e9d5a0a6db65634d26cde8af
Becca <rkinnee91@hotmail.com>
2023-10-05 22:57:12 -0700

    correct entire profile navigation. Made easier

--- src/Components/ProfileNav/ProfileNav.js
+++ src/Components/ProfileNav/ProfileNav.js
@@ -4,10 +4,10 @@ function ProfileNav () {
     return (
         <div className="profileNav">
             <div className="profileNav__left">
-                <button className="profileNav__left--nav">LEFT</button>
+                <button className="profileNav__button profileNav__left--nav">L</button>
             </div>
             <div className="profileNav__right">
-                <button className="profileNav__right--nav">RIGHT</button>
+                <button className="profileNav__button profileNav__right--nav">R</button>
             </div>    
         </div>
     )
