d1ab3122872aaf33aa9c5c8e216b73fe01bea175
Becca <rkinnee91@hotmail.com>
2023-10-06 23:11:08 -0700

    build out profileVideoCard structure. Make responsive @ 320px

--- src/Components/ProfileVideoCard/ProfileVideoCard.js
+++ src/Components/ProfileVideoCard/ProfileVideoCard.js
@@ -3,7 +3,9 @@ import './ProfileVideoCard.scss';
 function ProfileVideoCard () {
     return (
         <div className="profileVideoCard">
-            
+            <div className="profileVideoCard__videoCard">
+                
+            </div>
         </div>
     )
 }
