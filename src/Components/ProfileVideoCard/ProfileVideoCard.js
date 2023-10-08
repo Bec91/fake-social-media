a045cbccde4d15417be60be87c4fbb8af4a1b176
Becca <rkinnee91@hotmail.com>
2023-10-07 23:31:09 -0700

    follow tutorial for responsive images. built correctly. Need to add styling to video container.

--- src/Components/ProfileVideoCard/ProfileVideoCard.js
+++ src/Components/ProfileVideoCard/ProfileVideoCard.js
@@ -2,10 +2,37 @@ import './ProfileVideoCard.scss';
 
 function ProfileVideoCard () {
     return (
-        <div className="profileVideoCard">
-            <div className="profileVideoCard__videoCard">
-                
-            </div>
+        <div className="VideoCardContainer">
+            <ul className="VideoCardContainer__videoGallery">
+                <li className="">
+                    <img className="VideoCardContainer__video VideoCardContainer__overlay" src="https://source.unsplash.com/VWcPlbHglYc/640x416" alt=""/>
+                </li>
+                <li>
+                    <img className="VideoCardContainer__video VideoCardContainer__overlay" src="https://source.unsplash.com/e6FMMambeO4/640x416" alt=""/>
+                </li>
+                <li>
+                    <img className="VideoCardContainer__video VideoCardContainer__overlay" src="https://source.unsplash.com/klCiPmzUw0Y/640x416" alt=""/>
+                </li>
+                <li className="">
+                    <img className="VideoCardContainer__video VideoCardContainer__overlay" src="https://source.unsplash.com/VWcPlbHglYc/640x416" alt=""/>
+                </li>
+                <li>
+                    <img className="VideoCardContainer__video VideoCardContainer__overlay" src="https://source.unsplash.com/e6FMMambeO4/640x416" alt=""/>
+                </li>
+                <li>
+                    <img className="VideoCardContainer__video VideoCardContainer__overlay" src="https://source.unsplash.com/VWcPlbHglYc/640x416" alt=""/>
+                </li>
+                <li className="">
+                    <img className="VideoCardContainer__video VideoCardContainer__overlay" src="https://source.unsplash.com/VWcPlbHglYc/640x416" alt=""/>
+                </li>
+                <li>
+                    <img className="VideoCardContainer__video VideoCardContainer__overlay" src="https://source.unsplash.com/e6FMMambeO4/640x416" alt=""/>
+                </li>
+                <li>
+                    <img className="VideoCardContainer__video VideoCardContainer__overlay" src="https://source.unsplash.com/VWcPlbHglYc/640x416" alt=""/>
+                </li>
+
+            </ul>
         </div>
     )
 }
