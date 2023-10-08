57cc5e848f5a3df8b86e1071762c38f86f6799b5
Becca <rkinnee91@hotmail.com>
2023-10-07 23:53:37 -0700

    correct 'to-do' list. remove excess <li/> in profileVideoCard.js

--- src/Components/ProfileVideoCard/ProfileVideoCard.js
+++ src/Components/ProfileVideoCard/ProfileVideoCard.js
@@ -1,39 +1,14 @@
 import './ProfileVideoCard.scss';
 // Need to map videos into li
-// change image tag to video tag
+// change <img/> to <video/>
 // need thumbnail images for video tags
 function ProfileVideoCard () {
     return (
         <div className="VideoCardContainer">
             <ul className="VideoCardContainer__videoGallery">
-                <li className="">
-                    <img className="VideoCardContainer__video" src="https://source.unsplash.com/VWcPlbHglYc/640x416" alt=""/>
-                </li>
-                <li>
-                    <img className="VideoCardContainer__video" src="https://source.unsplash.com/e6FMMambeO4/640x416" alt=""/>
-                </li>
-                <li>
-                    <img className="VideoCardContainer__video" src="https://source.unsplash.com/klCiPmzUw0Y/640x416" alt=""/>
-                </li>
-                <li className="">
-                    <img className="VideoCardContainer__video" src="https://source.unsplash.com/VWcPlbHglYc/640x416" alt=""/>
-                </li>
-                <li>
-                    <img className="VideoCardContainer__video" src="https://source.unsplash.com/e6FMMambeO4/640x416" alt=""/>
-                </li>
                 <li>
                     <img className="VideoCardContainer__video" src="https://source.unsplash.com/VWcPlbHglYc/640x416" alt=""/>
                 </li>
-                <li className="">
-                    <img className="VideoCardContainer__video" src="https://source.unsplash.com/VWcPlbHglYc/640x416" alt=""/>
-                </li>
-                <li>
-                    <img className="VideoCardContainer__video" src="https://source.unsplash.com/e6FMMambeO4/640x416" alt=""/>
-                </li>
-                <li>
-                    <img className="VideoCardContainer__video" src="https://source.unsplash.com/VWcPlbHglYc/640x416" alt=""/>
-                </li>
-
             </ul>
         </div>
     )
