685bfcc54fb94c18b236d8b590010f5b2a8e05cb
Becca <rkinnee91@hotmail.com>
2023-10-14 20:52:40 -0700

    Initial set up of UploadVideo.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.scss';
 import ProfileCard from './Components/ProfileCard/ProfileCard';
 import ProfileNav from './Components/ProfileNav/ProfileNav';
-import ProfileVideoCard from './Components/ProfileVideoCard/ProfileVideoCard';
+// import ProfileVideoCard from './Components/ProfileVideoCard/ProfileVideoCard';
+import UploadVideo from './Components/UploadVideo/UploadVideo';
 
 function App() {
   return (
     <div className="App">
       <ProfileCard/>
       <ProfileNav/>
-      <ProfileVideoCard/>
+      <UploadVideo/>
+      {/* <ProfileVideoCard/> */}
     </div>
   );
 }
---
+++ src/Components/UploadVideo/UploadVideo.js
@@ -0,0 +1,10 @@
+import './UploadVideo.scss'
+
+function UploadVideo () {
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default UploadVideo;
