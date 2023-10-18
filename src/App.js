5315565f6252f404123f61976158d1b0dd770b43
Becca <rkinnee91@hotmail.com>
2023-10-17 21:19:26 -0700

    changing to a different branch. Build basic structure for uploading video comtent.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.scss';
 import ProfileCard from './Components/ProfileCard/ProfileCard';
 import ProfileNav from './Components/ProfileNav/ProfileNav';
-// import ProfileVideoCard from './Components/ProfileVideoCard/ProfileVideoCard';
-import UploadVideo from './Components/UploadVideo/UploadVideo';
+import ProfileVideoCard from './Components/ProfileVideoCard/ProfileVideoCard';
+// import UploadVideo from './Components/UploadVideo/UploadVideo';
 
 function App() {
   return (
     <div className="App">
       <ProfileCard/>
       <ProfileNav/>
-      <UploadVideo/>
-      {/* <ProfileVideoCard/> */}
+      {/* <UploadVideo/> */}
+      <ProfileVideoCard/>
     </div>
   );
 }
--- src/Components/UploadVideo/UploadVideo.js
+++ src/Components/UploadVideo/UploadVideo.js
@@ -1,10 +1,22 @@
 import './UploadVideo.scss'
+import React, { useState } from "react";
+
 
 function UploadVideo () {
-    return (
-        <div>
+    const [file, setFile] = useState();
 
+    function handleChange(e) {
+        console.log(e.target.files);
+        setFile(URL.createObjectURL(e.target.files[0]));
+    }
+
+    return (
+        <div className='uploadVideo'>
+            <h2 className='uploadVideo__title'>Add Video</h2>
+            <input type='file' onChange={handleChange} />
+            <img src={file} alt="adjsalf" />
         </div>
+        
     )
 }
 export default UploadVideo;
